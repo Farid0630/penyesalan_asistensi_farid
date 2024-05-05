96d4d2a5fa32fb3dd9203b4bd041c1c0a5133c94
Farid0630 <161107771+Farid0630@users.noreply.github.com>
2024-05-05 08:33:05 +0800

    Add files via upload

---
+++ farid/index.php
@@ -0,0 +1,34 @@
+<?php
+// include "";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post" >
+        <div style="border: 1px solid black; margin: auto">
+
+        <table border="1" cellspacing="0" cellpadding="5">
+            <caption>Daftar penjualan</caption>
+            <td><label for="">Masukkan Nama barang :</label></td>
+            <td><input type="text" placeholder="Nama"></td>
+        </tr>
+        <tr>
+            <td><label for="">Masukkan Harga :</label></td>
+            <td><input type="number" name="" id="" ></td>
+        </tr>
+        <tr>
+            <td><label for="">Masukkan Jenis barang</label></td>
+            <td><input type="text" ></td>
+        </tr>
+    </table>
+</div>
+    </form>
+</body>
+</html>
